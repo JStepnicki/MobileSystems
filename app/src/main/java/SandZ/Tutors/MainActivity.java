e387f6a00734964884da60304d7ccf710428906b
wzyndul <wojteckiz8630@gmail.com>
2023-11-12 18:25:00 +0100

    [ADDED] user login and register

--- app/src/main/java/SandZ/Tutors/MainActivity.java
+++ app/src/main/java/SandZ/Tutors/MainActivity.java
@@ -2,13 +2,47 @@ package SandZ.Tutors;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseAuth mAuth;
+    Button btnLogout;
+    TextView email, userType;
+    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        btnLogout = findViewById(R.id.btn_logout);
+        email = findViewById(R.id.user_email);
+        userType = findViewById(R.id.user_type);
+        user = mAuth.getCurrentUser();
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        if (user == null) {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        } else {
+            email.setText(user.getEmail());
+//            userType.setText(user.getDisplayName());
+        }
+
     }
 }
